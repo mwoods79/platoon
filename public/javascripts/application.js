a4eb8ccb8e8272fc9c9e936cc4556252ba70dca1
Chase McCarthy <ozone1015@gmail.com>
2010-10-31 03:53:30 -0500

    added login menu and nav menu bar

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
+	
   $('a.slide').click(function() {
     $('#container').slideUp('slow');
     return false;
+  });
+
+  $(".signin").click(function(e) {
+       e.preventDefault();
+       $("fieldset#signin_menu").toggle();
+       $(".signin").toggleClass("menu-open");
+  });
+
+   $("fieldset#signin_menu").mouseup(function() {
+      return false
+  });
+
+  $(document).mouseup(function(e) {
+      if($(e.target).parent("a.signin").length==0) {
+          $(".signin").removeClass("menu-open");
+          $("fieldset#signin_menu").hide();
+      }
+  });            
+  
+  $(".lavaLamp").lavaLamp({ 
+    fx: "backout", speed: 700 
   });
 });
