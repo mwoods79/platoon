e39d56a22fcdebe7a4b789f507e488f6a52ddb8e
Chase McCarthy <ozone1015@gmail.com>
2010-11-27 15:05:46 -0600

    Handeled player not found in stats service

--- app/models/player_stats.rb
+++ app/models/player_stats.rb
@@ -4,11 +4,16 @@ class PlayerStats
     doc = Nokogiri::HTML(open("http://www.bungie.net/Stats/Reach/CareerStats/default.aspx?player=#{CGI.escape(id.to_s)}&vc=3"))
     player = Hash.new
     player[:gamertag] = id
-    player[:kills] = doc.css('#ctl00_mainContent_killsLabel').first.content
-    player[:deaths] = doc.css('#ctl00_mainContent_deathsLabel').first.content
-    player[:games_played] = doc.css('ul.legend li.number').first.content
-    player[:kd] = (player[:kills].gsub(',', '').to_f / player[:deaths].gsub(',', '').to_f)
     
+    killsHtml = doc.css('#ctl00_mainContent_killsLabel').first
+    if killsHtml.nil?
+      player[:error] = "Player not found"
+    else
+      player[:kills] = doc.css('#ctl00_mainContent_killsLabel').first.content
+      player[:deaths] = doc.css('#ctl00_mainContent_deathsLabel').first.content
+      player[:games_played] = doc.css('ul.legend li.number').first.content
+      player[:kd] = (player[:kills].gsub(',', '').to_f / player[:deaths].gsub(',', '').to_f)
+    end
     player
   end
 end
